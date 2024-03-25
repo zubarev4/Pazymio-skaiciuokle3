c3c7f72d9ccab43432d1c650fe60ce559c822381
zubarev4 <147638474+zubarev4@users.noreply.github.com>
2024-03-25 20:06:22 +0200

    Add files via upload

---
+++ vectors/failu-generavimas.h
@@ -0,0 +1,22 @@
+#ifndef FAILU_GENERAVIMAS_H
+#define FAILU_GENERAVIMAS_H
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include "vektoriai.h"
+
+using namespace std;
+
+void writeCategorizedStudents(const vector<Student>& students, const string& filename);
+void generateFiles();
+void sortAndWriteToFile(const string& inputFilename);
+void generatingFinal();
+
+#endif
